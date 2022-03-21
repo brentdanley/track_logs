c409dccd4feb33b6a4074a31cf1de9e494f432b3
Brent Danley <brentdanley@gmail.com>
2022-03-20 21:53:19 -0400

    Adds support for kml files, color prop

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import FlightMap from './components/FlightMap';
 
 export default function App() {
   return (
-    <div className="track-map-container">
-      <FlightMap track={'saddleback_joni_20220317.gpx'} />
-    </div>
+    <>
+      <div className="track-map-container">
+        <FlightMap track={'saddleback_joni_20220317.gpx'} color={'#E9FB84'} />
+      </div>
+      <div className="track-map-container">
+        <FlightMap track={'beach_joni_20220305.kml'} />
+      </div>
+    </>
   );
 }
--- src/components/FlightMap.js
+++ src/components/FlightMap.js
@@ -1,13 +1,12 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import { gpx } from '@tmcw/togeojson';
+import { gpx, kml } from '@tmcw/togeojson';
 import { TwitterPicker } from 'react-color';
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoiYnJlbnRmZiIsImEiOiJjbDBjbHN0cDkwMGZmM2lueWF3NWxidXE3In0.suqzxkwsnKEaen07pmwVIw';
 
 const getCenter = (coords) => {
-  console.log(coords);
   let lngmin = coords[0][0];
   let lngmax = coords[0][0];
   let latmin = coords[0][1];
@@ -18,16 +17,20 @@ const getCenter = (coords) => {
     if (element[1] < latmin) latmin = element[1];
     if (element[1] > latmax) latmax = element[1];
   });
-  return [lngmin + (lngmax - lngmin) / 2, latmin + (latmax - latmin) / 2];
+  // return [lngmin + (lngmax - lngmin) / 2, latmin + (latmax - latmin) / 2];
+  return [
+    [lngmin, latmin],
+    [lngmax, latmax]
+  ];
 };
 
-const FlightMap = ({ track }) => {
+const FlightMap = ({ track, color }) => {
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [lng, setLng] = useState(-70.31);
   const [lat, setLat] = useState(43.65);
   const [zoom, setZoom] = useState(14);
-  const [trackColor, setTrackColor] = useState('#1d65b3');
+  const [trackColor, setTrackColor] = useState(color ?? '#1d65b3');
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
@@ -48,37 +51,44 @@ const FlightMap = ({ track }) => {
     });
 
     map.current.on('load', async () => {
-      const gpxData = await fetch(`/track_data/${track}`);
-      const gpxText = await gpxData.text();
-      const geoJSONdata = gpx(
-        new DOMParser().parseFromString(gpxText, 'text/xml')
-      );
+      const filetype = track.slice(-4, track.length);
+
+      const rawTrackData = await fetch(`/track_data/${track}`);
+      const xmlText = await rawTrackData.text();
+      let geoJSONdata;
+      if (filetype === '.gpx') {
+        geoJSONdata = gpx(new DOMParser().parseFromString(xmlText, 'text/xml'));
+      } else if (filetype === '.kml') {
+        geoJSONdata = kml(new DOMParser().parseFromString(xmlText, 'text/xml'));
+      }
 
       setLng(getCenter(geoJSONdata.features[0].geometry.coordinates));
       setLat(46.9447);
-      console.log(lat);
-      console.log(getCenter(geoJSONdata.features[0].geometry.coordinates));
-      map.current.flyTo({
-        center: getCenter(geoJSONdata.features[0].geometry.coordinates)
-      });
 
-      map.current.addSource('tracklayer', {
-        type: 'geojson',
-        data: geoJSONdata
-      });
+      map.current
+        .fitBounds(
+          getCenter(geoJSONdata.features[0].geometry.coordinates, {
+            padding: 20
+          })
+        )
 
-      map.current.addLayer({
-        id: 'track',
-        type: 'line',
-        source: 'tracklayer',
-        layout: {
-          visibility: 'visible'
-        },
-        paint: {
-          'line-color': trackColor,
-          'line-width': 4
-        }
-      });
+        .addSource('tracklayer', {
+          type: 'geojson',
+          data: geoJSONdata
+        })
+
+        .addLayer({
+          id: 'track',
+          type: 'line',
+          source: 'tracklayer',
+          layout: {
+            visibility: 'visible'
+          },
+          paint: {
+            'line-color': trackColor,
+            'line-width': 4
+          }
+        });
     });
   });
 
